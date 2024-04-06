6c0666ea9f819ae7689c1a87f53699ff79ad3dc9
nicknjagi <njaginicholas@gmail.com>
2024-04-06 20:44:23 +0300

    update components/Cta/index.js

--- components/Cta/index.js
+++ components/Cta/index.js
@@ -1,6 +1,15 @@
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <section className="bg-peach-primary my-6 mx-4 md:mx-0 px-4 md:px-24 py-16 lg:py-[72px] text-center lg:text-start rounded-2xl lg:flex lg:items-center lg:justify-between">
+      <div className="text-white mb-8">
+        <h3 className="text-[32px] md:text-[40px] font-medium max-w-[280px] md:max-w-[320px] mx-auto lg:mx-0 mb-4 leading-10">Let’s talk about your project</h3>
+        <p className="max-w-[430px] mx-auto lg:mx-0">
+          Ready to take it to the next level? Contact us today and find out how
+          our expertise can help your business grow.
+        </p>
+      </div>
+      <button className="btn-on-dark w-fit mx-auto lg:mx-0">GET IN TOUCH</button>
+    </section>
   )
 }
 export default Cta
