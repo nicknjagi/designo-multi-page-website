a1827ad35d2a800e172a7802f49a5cc02962e4f9
nicknjagi <njaginicholas@gmail.com>
2024-04-06 21:59:15 +0300

    update components/Logo/index.js

--- components/Logo/index.js
+++ components/Logo/index.js
@@ -1,14 +1,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { League_Spartan } from 'next/font/google'
 
-export const spartan = League_Spartan({subsets: ['latin']})
 
 const Logo = ({handleClick}) => {
   return (
     <Link href="/" onClick={handleClick} className="flex items-center gap-4">
       <Image src="/assets/shared/desktop/logo-dark.png" width={196} height={24} alt="logo" className='w-auto -ml-2'/>
-      {/* <span className={`text-2xl text-dark-grey font-bold tracking-[5px] pt-1 ${spartan.className} antialiased`}>DESIGNO</span> */}
     </Link>
   )
 }
