d1831d1b34ccc6c7fdd795da528416e3bb5f1268
nicknjagi <njaginicholas@gmail.com>
2024-04-06 21:59:07 +0300

    update components/Logo/index.js

--- components/Logo/index.js
+++ components/Logo/index.js
@@ -7,8 +7,8 @@ export const spartan = League_Spartan({subsets: ['latin']})
 const Logo = ({handleClick}) => {
   return (
     <Link href="/" onClick={handleClick} className="flex items-center gap-4">
-      <Image src="/assets/Oval.svg" width={32} height={32} alt="logo" className='w-auto -ml-2'/>
-      <span className={`text-2xl text-dark-grey font-bold tracking-[5px] pt-1 ${spartan.className} antialiased`}>DESIGNO</span>
+      <Image src="/assets/shared/desktop/logo-dark.png" width={196} height={24} alt="logo" className='w-auto -ml-2'/>
+      {/* <span className={`text-2xl text-dark-grey font-bold tracking-[5px] pt-1 ${spartan.className} antialiased`}>DESIGNO</span> */}
     </Link>
   )
 }
