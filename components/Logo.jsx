018e1767e4595b08fc06084cc2c001ffd5fdfd5a
Nick <njaginicholas@gmail.com>
2024-04-09 18:56:00 +0300

    chore: move and rename index.js to components/Logo.jsx

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -2,10 +2,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 
-const Logo = ({handleClick}) => {
+const Logo = ({handleClick, img}) => {
   return (
     <Link href="/" onClick={handleClick} className="flex items-center gap-4">
-      <Image src="/assets/shared/desktop/logo-dark.png" width={196} height={24} alt="logo" className='w-auto -ml-2'/>
+      <Image src={`/assets/shared/desktop/logo-${img}.png`} width={196} height={24} alt="logo" className='w-auto'/>
     </Link>
   )
 }
