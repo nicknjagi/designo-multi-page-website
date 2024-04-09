633badc86b958e8f51a492b0b68ab136111a993f
Nick <njaginicholas@gmail.com>
2024-04-09 22:59:43 +0300

    add background pattern

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -1,6 +1,17 @@
+import Image from "next/image"
+
 const Cta = () => {
   return (
-    <section className="bg-peach-primary my-6 mx-4 md:mx-0 px-6 md:px-24 py-16 lg:py-[72px] text-center lg:text-start rounded-2xl lg:flex lg:items-center lg:justify-between relative z-10 -mb-10 translate-y-36 md:mb-0 md:translate-y-20 lg:translate-y-24">
+    <section className="bg-peach-primary my-6 mx-4 md:mx-0 px-6 md:px-24 py-16 lg:py-[72px] text-center lg:text-start rounded-2xl lg:flex lg:items-center lg:justify-between relative z-30 -mb-10 translate-y-36 md:mb-0 md:translate-y-20 lg:translate-y-24 overflow-hidden">
+      <Image
+        src="/assets/shared/desktop/bg-pattern-call-to-action.svg"
+        width={362}
+        height={500}
+        className='absolute top-1/2 -translate-y-1/2 -right-10 md:right-20 lg:right-[272px] scale-[2.5] -z-10'
+        aria-hidden={true}
+        alt="background pattern"
+        priority
+      />
       <div className="text-white mb-8">
         <h3 className="text-[32px] md:text-[40px] font-medium max-w-[280px] md:max-w-[320px] mx-auto lg:mx-0 mb-4 leading-10">Let’s talk about your project</h3>
         <p className="max-w-[430px] lg:max-w-[460px] mx-auto lg:mx-0">
