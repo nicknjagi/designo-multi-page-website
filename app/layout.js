9d64b84fc8f8c7574abb630b6882b12931ea0591
Nick <njaginicholas@gmail.com>
2024-04-10 22:39:19 +0300

    update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Jost } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
 
 const jost = Jost({ subsets: ["latin"] });
 
