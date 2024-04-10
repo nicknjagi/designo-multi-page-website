dcb3a9f7a5700c9b4c82ea3903774d5917761f6b
Nick <njaginicholas@gmail.com>
2024-04-10 20:27:25 +0300

    update page.js

--- app/web-design/page.js
+++ app/web-design/page.js
@@ -1,6 +1,20 @@
+import Banner from "@/components/Banner"
+import Cta from "@/components/Cta"
+import WebCardLinks from "@/components/WebCardLinks"
+import WebCardsContainer from "@/components/WebCardsContainer"
+
 const WebDesign = () => {
+  const data = {
+    title: 'web design',
+    content:'We build websites that serve as powerful marketing tools and bring memorable brand experiences.'
+  }
   return (
-    <div>WebDesign</div>
+    <div className="text-center md:px-8 xl:px-0">
+      <Banner {...data}/>
+      <WebCardsContainer />
+      <WebCardLinks />
+      <Cta />
+    </div>
   )
 }
 export default WebDesign
