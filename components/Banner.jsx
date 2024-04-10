315e5c15b7ae8fc43a3d36b98368a26e291d4de5
Nick <njaginicholas@gmail.com>
2024-04-10 20:27:38 +0300

    feat: create Banner.jsx

---
+++ components/Banner.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+
+const Banner = ({title, content}) => {
+  return (
+    <section className="flex flex-col gap-6 justify-center text-white relative bg-peach-primary min-h-[320px] md:min-h-[252px] overflow-hidden md:rounded-2xl">
+        <h1 className="capitalize font-medium text-[32px] md:text-5xl  relative z-10">{title}</h1>
+        <p className="relative z-10 max-w-sm mx-auto">{content}</p>
+        <Image
+            src="/assets/shared/desktop/bg-pattern-small-circle.svg"
+            width={202}
+            height={202}
+            className='absolute top-1/2 -translate-y-1/2 -rotate-90 right-10 scale-[1.4] md:hidden'
+            aria-hidden={true}
+            alt="background pattern"
+        />
+        <Image
+            src="/assets/shared/desktop/bg-banner.svg"
+            width={202}
+            height={202}
+            className='absolute top-1/2 -translate-y-1/2 right-0 lg:-right-12 h-full w-full hidden md:block'
+            aria-hidden={true}
+            alt="background pattern"
+        />
+    </section>
+  )
+}
+export default Banner
