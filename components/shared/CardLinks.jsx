b1b56dc0d047d0db93ec48d2d873b3e9c271ee60
Nick <njaginicholas@gmail.com>
2024-04-12 21:13:58 +0300

    update CardLinks.jsx

--- components/shared/CardLinks.jsx
+++ components/shared/CardLinks.jsx
@@ -2,7 +2,7 @@ import CardLink from "./CardLink";
 
 const CardLinks = ({links}) => {
   return (
-    <section className="pt-24 px-4 md:px-0 flex flex-col lg:grid lg:grid-cols-2 gap-6">
+    <section className="my-[120px] lg:my-40 px-4 md:px-0 flex flex-col lg:grid lg:grid-cols-2 gap-6">
       {links.map((card, index) => {
         const {title, imgSrc, link} = card
         return (
