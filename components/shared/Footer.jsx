d900cac0d677b9261d55d2d3479be86165a493e1
Nick <njaginicholas@gmail.com>
2024-04-13 20:25:04 +0300

    update Footer.jsx

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -1,27 +1,31 @@
+'use client'
 import Link from 'next/link'
 import Logo from './Logo'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 
 const Footer = () => {
+  const pathname = usePathname()
+
   return ( 
-    <footer className="bg-dark-bg text-center md:text-left px-4 md:px-8 pt-64 md:pt-40 lg:pt-44">
-      <div class="max-w-[1111px] mx-auto">
+    <footer className={`bg-dark-bg text-center md:text-left  pb-16 md:pb-20 ${pathname === '/contact' ? 'pt-16 md:pt-20' : 'pt-64 md:pt-40 lg:pt-44'}`}>
+      <div class="max-w-[1111px] px-4 md:px-8 xl:px-0 mx-auto">
         <div className='flex flex-col md:flex-row md:justify-between items-center md:border-b border-[#979797] border-opacity-20 md:pb-10 md:mb-8'>
           <Logo img={'light'} />
           <hr className='border w-full border-[#979797] opacity-20 my-8 md:hidden'/>
-          <ul className='text-white text-sm flex flex-col md:flex-row gap-6 md:gap-10 mb-12 md:mb-0'>
-            <li className='border-b border-transparent hover:border-b-white transition ease-in-out origin-left'>
+          <ul className='text-white text-sm flex flex-col items-center md:flex-row gap-6 md:gap-10 mb-12 md:mb-0'>
+            <li className='w-fit border-b border-transparent hover:border-b-white transition ease-in-out origin-left'>
               <Link href="/about">OUR COMPANY</Link>
             </li>
-            <li className='border-b border-transparent hover:border-b-white transition ease-in-out origin-left'>
+            <li className='w-fit border-b border-transparent hover:border-b-white transition ease-in-out origin-left'>
               <Link href="/locations">LOCATIONS</Link>
             </li>
-            <li className='border-b border-transparent hover:border-b-white transition ease-in-out origin-left'>
+            <li className='w-fit border-b border-transparent hover:border-b-white transition ease-in-out origin-left'>
               <Link href="/contact">CONTACT</Link>
             </li>
           </ul>
         </div>
-        <div className='md:flex md:pb-20'>
+        <div className='md:flex'>
           <div className='text-darker-grey flex flex-col md:flex-row gap-12 lg:gap-48'>
             <div className='flex flex-col gap-[2px]'>
               <h4 className='font-bold'>Designo Central Office</h4>
@@ -34,7 +38,7 @@ const Footer = () => {
               <p>M : contact@designo.co</p>
             </div>
           </div>
-          <div className='flex justify-center gap-4 py-12 md:py-0 md:mt-auto md:ml-auto'>
+          <div className='flex justify-center gap-4 pt-12 md:py-0 md:mt-auto md:ml-auto'>
             <Link href="/">
               <Image
                 src="/assets/shared/desktop/icon-facebook.svg"
