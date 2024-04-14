e5a2621ec0a9e12d15a3219f27f719fec80b0121
Nick <njaginicholas@gmail.com>
2024-04-14 15:43:10 +0300

    update LocationsCard.jsx

--- components/LocationsCard.jsx
+++ components/LocationsCard.jsx
@@ -1,14 +1,26 @@
+'use client'
 import Image from "next/image"
+import dynamic from "next/dynamic"
+import { useMemo } from "react"
 
-const LocationCard = ({country, phone, mail, office, street, address, alt, img}) => {
+const LocationCard = ({country, phone, mail, office, street, address, data}) => {
+    const Map = useMemo(() => dynamic(
+        () => import('@/components/Map'),
+        { 
+          loading: () => <p className="flex items-center justify-center text-center h-full">Loading map</p>,
+          ssr: false
+        }
+      ), [])
+    
+
   return (
     <section id={country} className="flex flex-col lg:flex-row md:gap-6 lg:gap-[30px] lg:h-[326px]">
-        <div className="overflow-hidden md:rounded-2xl w-full lg:max-w-[350px] lg:h-full">
-            <Image
+        <div className="overflow-hidden md:rounded-2xl w-full lg:max-w-[350px] h-[300px] lg:h-full">
+            {/* <Image
                 src={`/assets/locations/tablet/${img}`}
                 width={689}
                 height={326}
-                className='w-full min-h-[320px] object-cover object-center lg:hidden'
+                className='w-full min-h-[320px] object-cover object-center hidden lg:hidden'
                 alt={alt}
             />
             <Image
@@ -17,11 +29,12 @@ const LocationCard = ({country, phone, mail, office, street, address, alt, img})
                 height={320}
                 className='w-full h-full object-cover object-center hidden lg:block'
                 alt={alt}
-            />
+            /> */}
+            <Map {...data}/>
         </div>
         <div className="relative bg-light-peach py-20 md:py-[88px] px-4 md:px-[72px] text-center md:text-left overflow-hidden md:rounded-2xl w-full">
-            <h2 className="text-peach-primary text-[32px] md:text-[40px] font-medium mb-6">{country}</h2>
-            <div className="flex flex-col md:flex-row gap-6 md:gap-36 lg:gap-24 xl:gap-36">
+            <h2 className="text-peach-primary text-[32px] md:text-[40px] font-medium mb-6 relative z-20">{country}</h2>
+            <div className="flex flex-col md:flex-row gap-6 md:gap-36 lg:gap-24 xl:gap-36 relative z-20">
                 <div>
                     <h4 className="font-bold">{office}</h4>
                     <p>{street}</p>
