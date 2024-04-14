9cf6a27c21ffb55cde9c1e33abe3a8acf9407900
Nick <njaginicholas@gmail.com>
2024-04-14 15:42:59 +0300

    feat: create Map.jsx

---
+++ components/Map.jsx
@@ -0,0 +1,21 @@
+import { MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import "leaflet-defaulticon-compatibility"
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+
+export default function MyMap(props) {
+  const { position, zoom } = props
+  console.log(position, zoom);
+
+  return <MapContainer className="map" center={position} zoom={zoom} scrollWheelZoom={true}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+}
